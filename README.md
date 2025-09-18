89d7fd277cda32b423d3d4f2cb203e0f5b47d897
neodix <namlem@gmail.com>
2025-09-18 10:14:18 +0400

    leave shared library libsecp256k1

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # ton-docker-ctrl
 
 Tested operating systems:
-* Ubuntu 20.04
 * Ubuntu 22.04
 * Ubuntu 24.04
-* Debian 11
 * Debian 12
 
 ## Prerequisites
@@ -18,12 +16,17 @@ Build environment variables (are configured in the .env file):
 * **GLOBAL_CONFIG_URL** - URL of the TON blockchain configuration (default: [Mainnet](https://ton.org/global.config.json))
 * **MYTONCTRL_VERSION** - MyTonCtrl build branch (default **master**)
 * **TELEMETRY** - Enable/Disable telemetry (default **true**)
-* **IGNORE_MINIMAL_REQS** - Ignore hardware requirements (default **true**) 
+* **IGNORE_MINIMAL_REQS** - Ignore hardware requirements (default **true**)
 * **MODE** - Install MyTonCtrl with specified mode (validator or liteserver, default **validator**)
-* **DUMP** - Use pre-packaged dump. Reduces duration of initial synchronization, but it takes time to download the dump. You can view the download status in the logs `docker compose logs -f`. (default **false**).
+* **DUMP** - Use pre-packaged dump. Reduces duration of initial synchronization, but it takes time to download the dump. You can view the download status in the logs `docker compose logs -f`. (default **false**)
+* **ARCHIVE_TTL** - Archive time-to-live in seconds for the validator (default **86400**)
+* **STATE_TTL** - State time-to-live in seconds for the validator (default **86400**)
+* **VERBOSITY** - Verbosity level for the validator engine (default **1**)
 
-## Run MyTonCtrl v2 in docker:
+## Run TON node with MyTonCtrl v2 in docker:
 * Run `docker run -d --name ton-node -v /mnt/data/ton-work1:/var/ton-work -it ghcr.io/ton-blockchain/ton-docker-ctrl:latest`
+## Run Archive node with MyTonCtrl v2 in docker:
+* Run `docker run -d --name ton-node -v /mnt/data/ton-work1:/var/ton-work -e MODE="liteserver" -e DUMP="true" -e ARCHIVE_TTL="1000000000" -e STATE_TTL="1000000000" -it ghcr.io/ton-blockchain/ton-docker-ctrl:latest`
 
 ## Build Docker image from sources and run MyTonCtrl v2:
 
@@ -41,7 +44,7 @@ Build environment variables (are configured in the .env file):
 
 ## Migrate non-Docker fullnode or validator to a dockerized MyTonCtrl v2
 
-Specify paths to TON binaries and sources, as well as to TON work directory, but most importantly to MyTonCtrl settings and wallets. 
+Specify paths to TON binaries and sources, as well as to TON work directory, but most importantly to MyTonCtrl settings and wallets.
 
 ```bash
 docker run -d --name ton-node --restart always \
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,6 +8,9 @@ cpus=$(nproc)
 memory=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
 CPUS=$(expr $(nproc) - 1)
 GLOBAL_CONFIG_URL=${GLOBAL_CONFIG_URL:-https://ton.org/testnet-global.config.json}
+ARCHIVE_TTL=${ARCHIVE_TTL:-86400}
+STATE_TTL=${STATE_TTL:-86400}
+VERBOSITY=${VERBOSITY:-1}
 
 echo "This machine has ${cpus} CPUs and ${memory}KB of Memory"
 if [ "$IGNORE_MINIMAL_REQS" != true ] && ([ "${cpus}" -lt 16 ] || [ "${memory}" -lt 64000000 ]); then
@@ -15,41 +18,49 @@ if [ "$IGNORE_MINIMAL_REQS" != true ] && ([ "${cpus}" -lt 16 ] || [ "${memory}" 
 	exit 1
 fi
 
-echo "Setting global config..."
-wget ${GLOBAL_CONFIG_URL} -O /usr/bin/ton/global.config.json
 
-URL="https://dump.ton.org"
-if [ ! -f /var/ton-work/db/dump_done ]; then
-  if [ "$DUMP" == true ] ; then
-    if [[ "$GLOBAL_CONFIG_URL" == *"testnet"* ]]; then
-       DUMP_NAME="latest_testnet"
-    else
-       DUMP_NAME="latest"
-    fi
-    echo "Start DownloadDump $DUMP_NAME"
-    DUMPSIZE=$(curl --silent ${URL}/dumps/${DUMP_NAME}.tar.size.archive.txt)
-    DISKSPACE=$(df -B1 --output=avail /var/ton-work | tail -n1)
-    NEEDSPACE=$(expr 3 '*' "$DUMPSIZE")
-    if [ "$DISKSPACE" -gt "$NEEDSPACE" ]; then
-      (curl --silent ${URL}/dumps/${DUMP_NAME}.tar.lz | pv --force | plzip -d -n${CPUS} | tar -xC /var/ton-work/db) 2>&1 | stdbuf -o0 tr '\r' '\n'
-      mkdir -p /var/ton-work/db/static /var/ton-work/db/import
-      chown -R validator:validator /var/ton-work/db
-      touch /var/ton-work/db/dump_done
-    echo "Done DownloadDump $DUMP_NAME"
-    else
-      echo "A minimum of $NEEDSPACE bytes of free disk space is required"
-      exit 1
-    fi
+echo "Downloading global config from ${GLOBAL_CONFIG_URL}"
+wget -q ${GLOBAL_CONFIG_URL} -O /usr/bin/ton/global.config.json
+
+if [ ! -f /var/ton-work/db/mtc_done ]; then
+  echo "Installing MyTonCtrl, version ${MYTONCTRL_VERSION}"
+  wget -q https://raw.githubusercontent.com/ton-blockchain/mytonctrl/${MYTONCTRL_VERSION}/scripts/install.sh -O /tmp/install.sh
+  if [ "$TELEMETRY" = false ]; then export TELEMETRY="-t"; else export TELEMETRY=""; fi
+  if [ "$IGNORE_MINIMAL_REQS" = true ]; then export IGNORE_MINIMAL_REQS="-i"; else export IGNORE_MINIMAL_REQS=""; fi
+  if [ "$DUMP" = true ]; then export DUMP="-d"; else export DUMP=""; fi
+  /bin/bash /tmp/install.sh ${TELEMETRY} ${IGNORE_MINIMAL_REQS} -b ${MYTONCTRL_VERSION} -m ${MODE} ${DUMP}
+  echo
+  echo "INSTALLED - updating and restarting services"
+  echo
+  ln -sf /proc/$$/fd/1 /usr/local/bin/mytoncore/mytoncore.log
+  ln -sf /proc/$$/fd/1 /var/log/syslog
+  sed -i 's/--logname \/var\/ton-work\/log//g' /etc/systemd/system/validator.service
+  sed -i 's/\[Service\]/\[Service\]\nStandardOutput=null\nStandardError=syslog/' /etc/systemd/system/validator.service
+  sed -i 's/\[Service\]/\[Service\]\nStandardOutput=null\nStandardError=syslog/' /etc/systemd/system/mytoncore.service
+  sed -i -e "s/--verbosity\s[[:digit:]]\+/--verbosity ${VERBOSITY}/g" /etc/systemd/system/validator.service
+  sed -i -e "s/--archive-ttl\s[[:digit:]]\+/--archive-ttl ${ARCHIVE_TTL}/g" /etc/systemd/system/validator.service
+
+  # Add --state-ttl parameter if not already present
+  if ! grep -q "\-\-state-ttl" /etc/systemd/system/validator.service; then
+      sed -i -e "s/--archive-ttl ${ARCHIVE_TTL}/--archive-ttl ${ARCHIVE_TTL} --state-ttl ${STATE_TTL}/g" /etc/systemd/system/validator.service
+  else
+      # Replace existing --state-ttl value if already present
+      sed -i -e "s/--state-ttl\s[[:digit:]]\+/--state-ttl ${STATE_TTL}/g" /etc/systemd/system/validator.service
   fi
+
+  touch /var/ton-work/db/mtc_done
+
+  echo "ReStarting validator"
+  systemctl restart validator
+  echo "ReStarting mytoncore"
+  systemctl restart mytoncore
+else
+  echo "MyTonCtrl already installed"
+  echo "Starting validator"
+#  systemctl start validator
+  echo "Starting mytoncore"
+#  systemctl start mytoncore
 fi
-
-echo "Setting processor cores"
-sed -i -e "s/--threads\s[[:digit:]]\+/--threads ${CPUS}/g" /etc/systemd/system/validator.service
-
-echo "Starting validator"
-systemctl start validator
-echo "Starting mytoncore"
-systemctl start mytoncore
 
 echo "Service started!"
 exec /usr/bin/systemctl
